99b12edd119d219a0e467016988c15cb2f528e14
Lydie Pluvinage <lydiepluvinage@yahoo.fr>
2021-07-06 02:11:26 +0200

    First version almost finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import BrainGame from './Game';
 
-function App() {
+const App = () => {
+  // App Component
+  // Displays header and BrainGame component
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Play with your brain
       </header>
+      <BrainGame />
     </div>
   );
-}
-
+};
 export default App;
---
+++ src/Game.js
@@ -0,0 +1,140 @@
+import './Game.css';
+import React, { useEffect, useState } from 'react';
+
+const BrainGame = () => {
+  // BrainGame component
+  // Sets the game and calculates scores
+  // Resets words and colors
+  // Deals with timer
+  const [gameId, setGameId] = useState(1);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [secondsLeft, setSecondsLeft] = useState(60);
+  const [validColor, setValidColor] = useState(
+    pickRandomColor(Object.entries(Colors))
+  );
+
+  // deals with timer
+  /*useEffect(() => {
+    if (secondsLeft > 0) {
+      const timerId = setTimeout(() => {
+        setSecondsLeft(secondsLeft - 1);
+      }, 1000);
+      return () => clearTimeout(timerId);
+    }
+  });*/
+
+  // Randomly chooses a color between the list of available colors
+  function pickRandomColor(array) {
+    const j = Math.floor(Math.random() * array.length);
+    return array[j][0];
+  }
+
+  // compares answer given and valid color
+  // increments correct answers if needed
+  // resets words
+  function checkAnswerAndReset(colorChosen) {
+    if (colorChosen === validColor) {
+      setCorrectAnswers(correctAnswers + 1);
+    }
+    setValidColor(pickRandomColor(Object.entries(Colors)));
+    setGameId(gameId + 1);
+  }
+
+  return (
+    <>
+      <Game
+        key={gameId}
+        validColor={validColor}
+        checkAnswer={checkAnswerAndReset}
+      />
+      <div className="answers">Bonnes réponses: {correctAnswers}</div>
+      <div className="timer">Secondes restantes: {secondsLeft}</div>
+    </>
+  );
+};
+
+// one instance of the game
+const Game = (props) => {
+  // function that shuffles an array
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  // function that mixes words and colors
+  function shuffleWords(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i][1], array[j][1]] = [array[j][1], array[i][1]];
+    }
+    return array;
+  }
+
+  return (
+    <div className="Game">
+      <div className="Game__Words">
+        {shuffleArray(shuffleWords(Object.entries(Colors))).map((color) => (
+          <ColorWord
+            key={color[0]}
+            name={color[0]}
+            color={color[1]}
+            selected={color[0] === props.validColor}
+          />
+        ))}
+      </div>
+      <div className="Game__Colors">
+        {shuffleArray(Object.entries(Colors)).map((color) => (
+          <ColorButton
+            key={color[0]}
+            name={color[0]}
+            backgroundColor={color[1]}
+            onClick={() => props.checkAnswer(color[0])}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// component displaying the colors
+const ColorButton = (props) => {
+  return (
+    <button
+      className="Game__Button"
+      style={{ backgroundColor: props.backgroundColor }}
+      onClick={props.onClick}
+    ></button>
+  );
+};
+
+// component displaying the game words
+const ColorWord = (props) => {
+  return (
+    <div
+      className="Game__Div"
+      style={{
+        border: props.selected === true ? '5px solid ' + props.color : 'none',
+      }}
+    >
+      <span style={{ color: props.color }}>{props.name}</span>
+    </div>
+  );
+};
+
+// list of available colors
+const Colors = {
+  Rouge: 'red',
+  Jaune: '#FFFF00',
+  Vert: 'green',
+  Bleu: 'blue',
+  Noir: 'black',
+  Orange: 'orange',
+  Violet: 'violet',
+  Marron: 'saddlebrown',
+  Turquoise: 'turquoise',
+};
+
+export default BrainGame;
